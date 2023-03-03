06518217073b35ff1fbaa08d2355cffacf42bec9
EC2 Default User <ec2-user@ip-172-31-46-99.ap-northeast-1.compute.internal>
2023-03-03 09:43:00 +0000

    Add 新規投稿機能

--- app/controllers/public/notes_controller.rb
+++ app/controllers/public/notes_controller.rb
@@ -1,8 +1,12 @@
 class Public::NotesController < ApplicationController
   def new
+    @note = Note.new
   end
 
   def create
+    @note = Note.new(note_params)
+    @note.save
+    redirect_to notes_path
   end
 
   def index
@@ -18,5 +22,12 @@ class Public::NotesController < ApplicationController
   end
 
   def destroy
+  end
+
+
+  private
+
+  def note_params
+    params.require(:note).permit(:title, :body, :image)
   end
 end
