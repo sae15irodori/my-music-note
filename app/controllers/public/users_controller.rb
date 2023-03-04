3aa43dd0b947bc4b52978c65f21b65aeb830cac7
EC2 Default User <ec2-user@ip-172-31-46-99.ap-northeast-1.compute.internal>
2023-03-04 07:15:57 +0000

    Add ユーザーの詳細画面

--- app/controllers/public/users_controller.rb
+++ app/controllers/public/users_controller.rb
@@ -3,6 +3,8 @@ class Public::UsersController < ApplicationController
   end
 
   def show
+    @user = User.find(params[:id])
+    @notes = @user.notes
   end
 
   def edit
--- app/models/user.rb
+++ app/models/user.rb
@@ -8,6 +8,17 @@ class User < ApplicationRecord
   has_many :favorites, dependent: :destroy
   has_many :note_comments, dependent: :destroy
 
+ has_one_attached :image
+
+  #def get_image
+    #unless image.attached?
+      #file_path = Rails.root.join('app/assets/images/no-image.jpg')
+      #image.attach(io: File.open(file_path), filename: 'default-image.jpg', content_type: 'image/jpeg')
+    #end
+    #image.variant(resize_to_limit: [100, 100]).processed
+  #end
+
+
 
   def self.guest#ゲストのレコードを作成するメソッド
     find_or_create_by!(email: 'aaa@aaa.com') do |user|#aaa@aaa.comがなければアドレスを自動生成？userへ格納
