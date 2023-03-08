aabbb85a01079c60360e3e00a60d44f208abd860
EC2 Default User <ec2-user@ip-172-31-46-99.ap-northeast-1.compute.internal>
2023-03-08 02:44:53 +0000

    Add ﾕｰｻﾞｰｱｶｳﾝﾄ削除している場合の処理

--- app/controllers/public/sessions_controller.rb
+++ app/controllers/public/sessions_controller.rb
@@ -25,16 +25,26 @@ class Public::SessionsController < Devise::SessionsController
   #   devise_parameter_sanitizer.permit(:sign_in, keys: [:attribute])
   # end
   before_action :authenticate_user!, except: [:top]
-  
+  before_action :user_state, only: [:create]
+
   def after_sign_in_path_for(resource)
     notes_path
   end
-  
+
   def after_sign_out_path_for(resource)
     root_path
   end
 
   protected
+
+  def user_state#ｱｶｳﾝﾄ削除しているか確認するﾒｿｯﾄﾞ
+    @user = User.find_by(email: params[:user][:email])#入力されたｱﾄﾞﾚｽを基にｱｶｳﾝﾄを取得
+    return if !@user#ｱｶｳﾝﾄ存在しない場合このﾒｿｯﾄﾞ終了
+      if @user.valid_password?(params[:user][:password]) && @user.is_deleted
+        flash[:notice] = "⚠このアカウントは削除済です。"
+        redirect_to new_user_session_path
+      end
+  end
 
   def configure_permitted_parameters
     devise_parameter_sanitizer.permit(:sign_up, keys: [:name])
