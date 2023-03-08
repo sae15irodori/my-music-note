2aceac4b073ddce67ad7661b1e04927bf4c127d0
EC2 Default User <ec2-user@ip-172-31-46-99.ap-northeast-1.compute.internal>
2023-03-08 04:21:01 +0000

    Delete コメントアウト

--- app/controllers/public/users_controller.rb
+++ app/controllers/public/users_controller.rb
@@ -38,7 +38,6 @@ class Public::UsersController < ApplicationController
 
     def withdrawal
       @user = User.find(params[:id])
-      # is_deletedカラムをtrueに変更することにより削除フラグを立てる
       @user.update(is_deleted: true)
       reset_session
       flash[:notice] = "退会処理が完了しました"
