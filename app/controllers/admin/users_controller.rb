f458656c493c122ccb448a9b5cdb8af4573b88d8
EC2 Default User <ec2-user@ip-172-31-46-99.ap-northeast-1.compute.internal>
2023-03-08 01:25:47 +0000

    Add 管理者側 ﾕｰｻﾞｰ詳細画面

--- app/controllers/admin/users_controller.rb
+++ app/controllers/admin/users_controller.rb
@@ -1,19 +1,21 @@
 class Admin::UsersController < ApplicationController
   def index
+    @users = User.all
   end
 
   def show
+    @user = User.find(params[:id])
+    @notes = @user.notes
   end
 
   def edit
+    @user = User.find(params[:id])
   end
+
 
   def update
   end
 
   def unsubscribe
-  end
-
-  def update
   end
 end
