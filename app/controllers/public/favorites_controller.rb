4a8026e15dee8ce522ea930f2396992371f4979d
EC2 Default User <ec2-user@ip-172-31-46-99.ap-northeast-1.compute.internal>
2023-03-17 08:00:15 +0000

    Update いいね機能非同期通信

--- app/controllers/public/favorites_controller.rb
+++ app/controllers/public/favorites_controller.rb
@@ -4,14 +4,12 @@ class Public::FavoritesController < ApplicationController
     @note = Note.find(params[:note_id])#いいねする投稿を取得
     favorite = current_user.favorites.new(note_id: @note.id)#ﾛｸﾞｲﾝﾕｰｻﾞｰのfavoritesﾚｺｰﾄﾞを用意note_iにdいいねする投稿のidを渡す
     favorite.save
-    redirect_to request.referer
   end
 
   def destroy
     @note = Note.find(params[:note_id])
     favorite = current_user.favorites.find_by(note_id: @note.id)
     favorite.destroy
-    redirect_to request.referer
   end
 
   private
