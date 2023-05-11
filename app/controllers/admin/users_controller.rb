6e93eb43b075a681ee8418bc5dd0bb0a9647f508
EC2 Default User <ec2-user@ip-172-31-46-99.ap-northeast-1.compute.internal>
2023-05-11 07:02:56 +0000

    Update 会員側退会処理アクション

--- app/controllers/admin/users_controller.rb
+++ app/controllers/admin/users_controller.rb
@@ -27,8 +27,7 @@ class Admin::UsersController < ApplicationController
   def withdrawal
     @user = User.find(params[:id])
     @user.update(is_deleted: true)
-    reset_session
-    flash[:notice] = "退会処理が完了しました"
+    flash[:notice] = "#{@user.name}さんの退会処理が完了しました"
     redirect_to admin_users_path
   end
 
