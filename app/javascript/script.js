98023bab0cbb00bf33150ffaab298aac667bf31d
EC2 Default User <ec2-user@ip-172-31-46-99.ap-northeast-1.compute.internal>
2023-06-05 10:08:25 +0000

    Fix ハンバーガーメニューjavascript

--- app/javascript/script.js
+++ app/javascript/script.js
@@ -1,6 +1,6 @@
 document.addEventListener("turbolinks:load", function() {
 $(function() {
-  $('.icon').on('click', function(event) {
+  $('.icon span').on('click', function(event) {
     $(this).toggleClass('active');
     $('.header-nav-sm').fadeToggle();
     event.preventDefault();
